bade26b0ab9cfe671d975f6d4abaceda42056aa2
unknown <huseynamirahmadov@gmail.com>
2026-03-02 10:40:47 +0400

    setup redux toolkit with typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './assets/style/index.css'
+import App from './App'
+import { Provider } from 'react-redux'
+import { store } from './redux/app/store.ts'
 
-createRoot(document.getElementById('root')!).render(<App />)
+const container = document.getElementById('root')
+
+if (container) {
+    const root = createRoot(container)
+
+    root.render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    )
+} else {
+    throw new Error(
+        "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+    )
+}
