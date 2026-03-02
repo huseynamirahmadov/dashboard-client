e243e15b643051c6ebae9ddce2b340cda7b9c107
unknown <huseynamirahmadov@gmail.com>
2026-03-02 23:46:27 +0400

    api integration

--- src/pages/Trades.tsx
+++ src/pages/Trades.tsx
@@ -1,32 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import React from 'react';
 import type { TradeData } from '../types/trade.types';
 import TradeCard from '../components/TradeCard';
+import axios from 'axios';
 
 const TradesComponent: React.FC = () => {
-  const [trades, setTrades] = useState<TradeData[]>([
-    {
-      id: 1,
-      date: '2026-03-02',
-      symbol: 'MNQH26',
-      direction: 'Long',
-      durationSeconds: 90,
-      quantity: 2,
-      risk: 200,
-      riskReward: 1,
-      range: 50,
-      pnl: 200,
-      fee: -1.48,
-      status: 'TP',
-      model: 'ARK',
-      screenshots: ['screenshot1.png'],
-    },
-  ]);
+  const [trades, setTrades] = useState<TradeData[]>([]);
+
+  useEffect(() => {
+    axios('http://127.0.0.1:8000/api/trades')
+      .then(res => setTrades(res.data))
+      .catch(err => console.log(err))
+  },[])
+  console.log(trades)
 
   return <div className='flex items-center justify-between flex-wrap'>
     {
       trades?.map(item => {
-        return <TradeCard item={item} />
+        return <TradeCard key={item.id} item={item} />
       })
     }
   </div>;
